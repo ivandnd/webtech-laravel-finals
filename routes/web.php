e6cb4c67af930d7ffac0193dc0c58107673e852a
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-06-22 13:57:18 +0800

    Make profile page and events page only be accessed by authenticated users

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'homePage'])->name('home');
 
-Route::get('/profile', [PageController::class, 'profilePage'])->name('profile');
+Route::get('/profile', [PageController::class, 'profilePage'])->middleware('auth')->name('profile');
 
-Route::get('/events', [PageController::class, 'eventsPage'])->name('events');
+Route::get('/events', [PageController::class, 'eventsPage'])->middleware('auth')->name('events');
 
 Route::get('/login', [PageController::class, 'loginPage'])->name('login');
 
