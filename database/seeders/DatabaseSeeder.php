f88fa11f51d3993a8f943ee37b07f92fa63c28ca
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-06-22 13:32:43 +0800

    Add requirements for database seeder
    based on final exam

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Administrator',
+            'username' => 'admin',
+            'password' => 'password'
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Staff',
+            'username' => 'staff',
+            'password' => 'password'
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Clerk',
+            'username' => 'clerk',
+            'password' => 'password'
+        ]);
     }
 }
