7f2ae2d87d8f8eb3a72d855a32543dd91946440e
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-06-22 13:29:47 +0800

    Add requirements for migration

--- database/migrations/2023_06_22_052501_create_profiles_table.php
+++ database/migrations/2023_06_22_052501_create_profiles_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->enum('gender', ['Male', 'Female']);
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
--- database/migrations/2023_06_22_052709_create_tickets_table.php
+++ database/migrations/2023_06_22_052709_create_tickets_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('password');
+            $table->string('ticket_number');
+            $table->enum('ticket_type', ['VIP', 'VIP2', 'Regular']);
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
